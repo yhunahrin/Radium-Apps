0c06a5c7c843af5a316035a5832469295d5a6dcc
yhunahrin <thienlangliettruyen@gmail.com>
2021-02-08 16:11:43 +0100

    Update ShaderEditorWidget.cpp

--- ShaderEditor/ShaderEditorWidget.cpp
+++ ShaderEditor/ShaderEditorWidget.cpp
@@ -10,10 +10,18 @@
 // include the custom material definition
 #include <Engine/Renderer/Material/RawShaderMaterial.hpp>
 
+#include <QColorDialog>
+#include <QComboBox>
+#include <QFileDialog>
+#include <QSettings>
+#include <QToolButton>
+#include <iostream>
+#include <QMessageBox>
 #include <QPushButton>
 #include <QString>
 #include <QTextEdit>
-
+#include <QMenu>
+#include <QImageReader>
 ShaderEditorWidget::ShaderEditorWidget(
                                 const std::string& v,
                                 const std::string& f,
@@ -28,11 +36,12 @@ ShaderEditorWidget::ShaderEditorWidget(
     _paramProvider( paramProvider )
 {
     ui->setupUi(this);
-
     ui->_vertShaderEdit->setPlainText( QString::fromStdString( v ) );
     ui->_fragShaderEdit->setPlainText( QString::fromStdString( f ) );
 
     connect( ui->_compileShaders, &QPushButton::clicked, this, &ShaderEditorWidget::updateShadersFromUI );
+    connect( ui->_addData, &QPushButton::clicked, this, &ShaderEditorWidget::loadData);
+    connect( ui->_showImage, &QPushButton::clicked, this, &ShaderEditorWidget::showImage);
 }
 
 ShaderEditorWidget::~ShaderEditorWidget()
@@ -40,16 +49,47 @@ ShaderEditorWidget::~ShaderEditorWidget()
     delete ui;
 }
 
-void
-ShaderEditorWidget::updateShadersFromUI() 
+void ShaderEditorWidget::updateShadersFromUI() 
 {
     using ShaderConfigType = std::vector<std::pair<Ra::Engine::ShaderType, std::string>> ;
 
     const ShaderConfigType config {
-        {Ra::Engine::ShaderType::ShaderType_VERTEX,   ui->_vertShaderEdit->toPlainText().toStdString()},
-        {Ra::Engine::ShaderType::ShaderType_FRAGMENT, ui->_fragShaderEdit->toPlainText().toStdString()}};
+       {Ra::Engine::ShaderType::ShaderType_VERTEX,   ui->_vertShaderEdit->toPlainText().toStdString()},
+       {Ra::Engine::ShaderType::ShaderType_FRAGMENT, ui->_fragShaderEdit->toPlainText().toStdString()}};
 
     auto mat           = static_cast<Ra::Engine::RawShaderMaterial*>( _ro->getMaterial().get() );
     mat->updateShaders( config, _paramProvider );
     _renderer->buildRenderTechnique( _ro.get() );
 }
+
+void ShaderEditorWidget::loadData()
+{
+    QImageReader reader;
+    QSettings settings;
+    QMessageBox msgBox;
+    msgBox.setText("Selection Files(Yes) or Folder(No)?");
+    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    msgBox.setDefaultButton(QMessageBox::Yes);
+    int res = msgBox.exec();
+   ;
+    if (res==QMessageBox::Yes){
+        QString path  = settings.value( "files/load", QDir::homePath()).toString();
+        QStringList pathList = QFileDialog::getOpenFileNames( this, "Open Files", path,tr("Image files (*.jpg *.png *.bmp)") );
+        ui->_listPath->addItems(pathList);
+    }
+    else if (res == QMessageBox::No){
+        QDir directory = QFileDialog::getExistingDirectory(this, "Select Directory");
+        QStringList pathList = directory.entryList(QStringList() << "*.jpg" << "*.JPG" << "*.png" << "*.PNG" << "*.bmp" << "*.BMP",QDir::Files);
+        QStringList pathListFull;
+        for (int i=0; i<pathList.size();i++){
+            pathListFull.append(directory.path()+"/"+pathList.at(i));
+        }
+        ui->_listPath->addItems(pathListFull);
+    }
+}
+
+void ShaderEditorWidget::showImage()
+{
+ std::cout<<ui->_listPath->currentText().toStdString()<<std::endl;
+}
+
