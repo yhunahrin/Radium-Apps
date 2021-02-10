51ccb52a47e443cefbfbd78f549cd6c4ec930f20
yhunahrin <thienlangliettruyen@gmail.com>
2021-02-10 16:09:15 +0100

    Update MyParameterProvider.hpp

--- ShaderEditor/MyParameterProvider.hpp
+++ ShaderEditor/MyParameterProvider.hpp
@@ -19,10 +19,10 @@ class MyParameterProvider : public Ra::Engine::ShaderParameterProvider
         // The name of the parameter corresponds to the shader's uniform name.
         m_renderParameters.addParameter( "aColorUniform", m_colorParameter );
         m_renderParameters.addParameter( "aScalarUniform", m_scalarParameter );
-        Ra::Engine::TextureManager tmp;
         Ra::Engine::TextureParameters tmp1;
+
         tmp1.name = m_path;
-        m_textureParameter = tmp.loadTexture(tmp1);
+        m_textureParameter = m_textureManager.loadTexture(tmp1);
         m_renderParameters.addParameter( "aTextureUniform", m_textureParameter);
     }
     std::string getPath(){
@@ -33,20 +33,18 @@ class MyParameterProvider : public Ra::Engine::ShaderParameterProvider
     }
     void setOrComputeTheParameterValues() {
         // client side computation of the parameters, e.g.
-        m_colorParameter  = Ra::Core::Utils::Color::Red();
-        m_scalarParameter = .1_ra;
-        m_path = "C:/Users/aduongng/Desktop/TP_VO/20703106.jpg";
-        Ra::Engine::TextureManager tmp;
+     //  m_colorParameter  = Ra::Core::Utils::Color::Red();
+      //  m_scalarParameter = .1_ra;
+        m_path = "C:\\Users\\aduongng\\Desktop\\TP_VO\\20703106.jpg";
         Ra::Engine::TextureParameters tmp1;
         //tmp1.name = "C:\\Users\\aduongng\\Deskto\\TP_VO\\20703106.jpg";
-
         tmp1.name = m_path;
         //tmp1 = tmp.addTexture(tmp1.name, tmp1.width, tmp1.height,tmp1.texels);
       //  tmp.loadTextureImage(tmp1);
-         m_textureParameter = tmp.loadTexture(tmp1);
+         m_textureParameter = m_textureManager.loadTexture(tmp1);
         // m_textureParameter = tmp.getOrLoadTexture(tmp1);
 
-      //   m_textureParameter = tmp1.name = "C:\\Users\\aduongng\\Desktop\\TP_VO\\20703106.jpg";
+      //   m_textureParameter = tmp1.name = "C:\\Users\\aduongng\\Desktop\\TP_VO\\20703106.png";
        //tmp.getOrLoadTexture(tmp1);
        /* int w,h;
         unsigned char * texture_data = stbi_load("C:/Users/aduongng/Desktop/TP_VO/20703106.jpg", &w, &h, nullptr, 4);
@@ -57,8 +55,9 @@ class MyParameterProvider : public Ra::Engine::ShaderParameterProvider
   private:
     Ra::Core::Utils::Color m_colorParameter {Ra::Core::Utils::Color::Green()};
     Scalar m_scalarParameter {1};
+    Ra::Engine::TextureManager m_textureManager{};
     Ra::Engine::Texture * m_textureParameter{};
-    std::string m_path {"C:\\Users\\aduongng\\Desktop\\TP_VO\\20703106.jpg"};
+    std::string m_path {""};
 };
 
 
