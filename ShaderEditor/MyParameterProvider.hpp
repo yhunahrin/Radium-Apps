a376a4b6949ee8fe3de0264b735b817c9b1560e5
yhunahrin <thienlangliettruyen@gmail.com>
2021-03-02 12:05:16 +0100

    Update MyParameterProvider.hpp

--- ShaderEditor/MyParameterProvider.hpp
+++ ShaderEditor/MyParameterProvider.hpp
@@ -11,7 +11,7 @@ using ShaderConfigType = std::vector<std::pair<Ra::Engine::ShaderType, std::stri
 
 class MyParameterProvider :public Ra::Engine::ShaderParameterProvider
 {
-  public:
+public:
     MyParameterProvider() {}
     ~MyParameterProvider() {}
     void updateGL() override {
@@ -19,21 +19,30 @@ class MyParameterProvider :public Ra::Engine::ShaderParameterProvider
         // The name of the parameter corresponds to the shader's uniform name.
         m_renderParameters.addParameter( "aColorUniform", m_colorParameter );
         m_renderParameters.addParameter( "aScalarUniform", m_scalarParameter );
+        //Loading a texture (meant to be moved in an other class enabling multiple loading)
+       // auto texManager = Ra::Engine::TextureManager{};
+     /*   auto texParam = texManager.addTexture("C:/Users/aduongng/Desktop/project/ex.bmp",800,800,nullptr);
+        auto tex = texManager.getOrLoadTexture(texParam);
+        tex->initializeGL();
+        tex->bind(0_ra);
+        m_renderParameters.addParameter(  "aTextureUniform", tex);*/
         m_renderParameters.addParameter( "aTextureUniform",  m_textureManager.getOrLoadTexture(m_textureManager.addTexture(this->getPath(),0,0,nullptr)));
     }
     std::string getPath(){
-        return m_path;
-    }
+           return m_path;
+       }
     void setOrComputeTheParameterValues(std::string path) {
         // client side computation of the parameters, e.g.
         m_colorParameter  = Ra::Core::Utils::Color::Red();
-        m_scalarParameter = .1_ra;
-        m_path = path;
+        m_scalarParameter = .5_ra;
+      //  m_textunit = 0_ra;
+         m_path = path;
     }
 
   private:
     Ra::Core::Utils::Color m_colorParameter {Ra::Core::Utils::Color::Green()};
     Scalar m_scalarParameter {1};
+    //Ra::Engine::TextureManager texManager;
     Ra::Engine::TextureManager m_textureManager{};
     std::string m_path {""};
 };
