0cbaec91d1636737016db79b871a6e4180bc65cc
yhunahrin <thienlangliettruyen@gmail.com>
2021-03-11 16:08:26 +0100

    Update ShaderEditorWidget.cpp

--- ShaderEditor/ShaderEditorWidget.cpp
+++ ShaderEditor/ShaderEditorWidget.cpp
@@ -1,4 +1,8 @@
-#include "LoadFolderWidget.h"
+#include "ShaderEditorWidget.hpp"
+#include "ui_ShaderEditorWidget.h"
+
+#include "MyParameterProvider.hpp"
+
 #include <Engine/Renderer/Renderer.hpp>
 #include <Engine/Renderer/RenderObject/RenderObjectManager.hpp>
 #include <Engine/Renderer/RenderObject/RenderObject.hpp>
@@ -26,219 +30,92 @@
 #include <QMenu>
 #include <QToolBar>
 #include <QSlider>
-#include <fstream>
+ShaderEditorWidget::ShaderEditorWidget(
+                                const std::string& v,
+                                const std::string& f,
+                                std::shared_ptr< Ra::Engine::RenderObject > ro,
+                                Ra::Engine::Renderer * renderer,
+                                std::string path,
+                                std::shared_ptr< MyParameterProvider > paramProvider,
+                                QWidget *parent) :
+    QWidget( parent ),
+    ui( new Ui::ShaderEditorWidget ),
+    _ro( ro ),
+    _renderer( renderer ),
+    _path(path),
+    _paramProvider(paramProvider)
 
-LoadFolderWidget::LoadFolderWidget( std::shared_ptr< Ra::Engine::RenderObject > ro,
-                                                            Ra::Engine::Renderer * renderer,
-                                                            std::string pathRaw,
-                                                            std::string pathUnet,
-                                                            std::shared_ptr< MyParameterProvider > paramProvider,
-                                                            QWidget *parent) :
-                                QWidget( parent ),
-                                ui( new Ui::LoadFolderWidget),
-                                _ro( ro ),
-                                _renderer( renderer ),
-                                _pathRaw(pathRaw),
-                                _pathUnet(pathUnet),
-                                _paramProvider(paramProvider)
 {
     ui->setupUi(this);
-    ui->imageOrgine->setEnabled(false);
-    ui->loadUnet->setEnabled(false);
+    ui->_vertShaderEdit->setPlainText( QString::fromStdString( v ) );
+    ui->_fragShaderEdit->setPlainText( QString::fromStdString( f ) );
+    QPixmap pen("C:\\Users\\aduongng\\Desktop\\project\\App\\Radium-Apps\\ShaderEditor\\pencil.png");
+    QPixmap colorBlack("C:\\Users\\aduongng\\Desktop\\project\\App\\Radium-Apps\\ShaderEditor\\black.png");
+    QPixmap colorWhite("C:\\Users\\aduongng\\Desktop\\project\\App\\Radium-Apps\\ShaderEditor\\white.png");
+    QPixmap large("C:\\Users\\aduongng\\Desktop\\project\\App\\Radium-Apps\\ShaderEditor\\large1.png");
+    QPixmap petit("C:\\Users\\aduongng\\Desktop\\project\\App\\Radium-Apps\\ShaderEditor\\small1.png");
+    /*add
+    QToolBar *toolbarDraw = addToolBar("Draw");
+    pencil = toolbarDraw->addAction(QIcon(pen), "pencil");
+    QAction *black = toolbarDraw->addAction(QIcon(colorBlack), "black");
+    QAction *white = toolbarDraw->addAction(QIcon(colorWhite), "white");
+    pencil->setEnabled(false);
+    toolbarDraw->addSeparator();
+    QAction *_toolbarDraw = toolbarDraw->toggleViewAction();
+    ui->menuEdit->addAction(_toolbarDraw);
+    QToolBar *toolbarZoom = addToolBar("Zoom");
+    zLarge = toolbarZoom->addAction(QIcon(large), "zoomLarge");
+    zSmall = toolbarZoom->addAction(QIcon(petit), "zoomSmall");*/
 
-    connect( ui->buttonLoad, &QPushButton::clicked, this, &LoadFolderWidget::enableLoadData );
-    connect( ui->buttonShow, &QPushButton::clicked, this, &LoadFolderWidget::enableShowImage);
-    connect( ui->buttonJson, &QPushButton::clicked, this, &LoadFolderWidget::enableSaveJson);
-    connect( ui->buttonRunImage, &QPushButton::clicked, this, &LoadFolderWidget::runImage);
-    connect( ui->buttonRunUnet, &QPushButton::clicked, this, &LoadFolderWidget::runUnet);
+    connect( ui->_compileShaders, &QPushButton::clicked, this, &ShaderEditorWidget::updateShadersFromUI );
+    connect( ui->_addData, &QPushButton::clicked, this, &ShaderEditorWidget::loadData);
+    connect( ui->_showImage, &QPushButton::clicked, this, &ShaderEditorWidget::showImage);
 }
 
-LoadFolderWidget::~LoadFolderWidget()
+ShaderEditorWidget::~ShaderEditorWidget()
 {
     delete ui;
 }
 
-void LoadFolderWidget::enableLoadData()
+void ShaderEditorWidget::updateShadersFromUI() 
 {
-    choisTypeRun = LOADDATA;
-    ui->imageOrgine->setEnabled(true);
-    ui->loadUnet->setEnabled(true);
+    using ShaderConfigType = std::vector<std::pair<Ra::Engine::ShaderType, std::string>> ;
+
+    const ShaderConfigType config {
+       {Ra::Engine::ShaderType::ShaderType_VERTEX,   ui->_vertShaderEdit->toPlainText().toStdString()},
+       {Ra::Engine::ShaderType::ShaderType_FRAGMENT, ui->_fragShaderEdit->toPlainText().toStdString()}};
+    auto mat           = static_cast<Ra::Engine::RawShaderMaterial*>( _ro->getMaterial().get() );
+    mat->updateShaders( config, _paramProvider );
+    _renderer->buildRenderTechnique( _ro.get() );
 }
 
-void LoadFolderWidget::enableShowImage()
+void ShaderEditorWidget::loadData()
 {
-    choisTypeRun = SHOWIMAGE;
-    ui->imageOrgine->setEnabled(true);
-    ui->loadUnet->setEnabled(true);
-}
-
-void LoadFolderWidget::enableSaveJson()
-{
-    choisTypeRun = SAVEJSON;
-    ui->imageOrgine->setEnabled(true);
-    ui->loadUnet->setEnabled(true);
-}
-void LoadFolderWidget::runImage()
-{
+    QImageReader reader;
+    QSettings settings;
     QMessageBox msgBox;
-    switch(choisTypeRun){
-    case LOADDATA:{
-        msgBox.setText("Selection Folder Of Original Image?");
-        msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-        msgBox.setDefaultButton(QMessageBox::No); 
-        int res = msgBox.exec();
-        if (res==QMessageBox::Yes){
-            ui->boxListImageOrigine->clear();
-            QDir directory = QFileDialog::getExistingDirectory(this, "Select Directory");
-            QStringList nameList = directory.entryList(QStringList() << "*.jpg" << "*.JPG" << "*.png" << "*.PNG" << "*.bmp" << "*.BMP",QDir::Files);
-            std::string textDataset="{ \"datasetinfo\" :\n  {\n       \"name\" : \"";
-            textDataset+=directory.dirName().toStdString();
-            textDataset+="\",\n";
-            textDataset+="       \"location\" : \"";
-            textDataset+=directory.path().toStdString();
-            textDataset+="\",\n";
-            textDataset+="       \"contains\" : \"";
-            textDataset+=std::to_string(nameList.size());
-            textDataset+=" files\",\n";
-            textDataset+="       \"images\" : [\n";
-            for (int i=0; i<nameList.size();i++){
-                   pathListRawFull.append(directory.path()+"/"+nameList.at(i));
-                   textDataset+="       {\n";
-                   textDataset+="            \"name\" : \"";
-                   char *p;
-                   std::string tmp = nameList.at(i).toStdString();
-                   char *tmp1 = const_cast<char*>(tmp.c_str());
-                   p = strtok(tmp1, ".");
-                   textDataset+=p;
-                   textDataset+="\",\n";
-                   textDataset+="            \"itemtype\" : \"";
-                   p = strtok(NULL, "\eof");
-                   textDataset+=p;
-                   textDataset+="\",\n";
-                   QImageReader reader;
-                   QSettings settings;
-                   reader.setFileName(directory.path()+"/"+nameList.at(i));
-                   QImage image = reader.read();
-                   textDataset+="            \"size\" : \"";
-                   textDataset+=std::to_string(image.sizeInBytes()/1024);
-                   textDataset+=" KB\",\n";
-                   textDataset+="            \"dimensiones\" : \"";
-                   textDataset+=std::to_string(image.width());
-                   textDataset+=" x ";
-                   textDataset+=std::to_string(image.height());
-                   textDataset+="\"\n";
-                   if(i!=nameList.size()-1){
-                        textDataset+="       },\n";
-                        textDataset+="\n";
-                    }
-                   else
-                       textDataset+="       }\n";
-             }
-            textDataset+="               ]\n";
-            textDataset+="  }\n";
-            textDataset+="}";
-            ui->textDatasetJson->setText(QString::fromStdString(textDataset));
-            ui->boxListImageOrigine->addItems(nameList);
-        }
-        else if (res == QMessageBox::No){
-            QMessageBox msgBox1;
-            msgBox1.setText("Nothing Selected !!!");
-            msgBox1.exec();
-        }
+    msgBox.setText("Selection Files(Yes) or Folder(No)?");
+    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    msgBox.setDefaultButton(QMessageBox::Yes);
+    int res = msgBox.exec();
+    if (res==QMessageBox::Yes){
+        QString path  = settings.value( "files/load", QDir::homePath()).toString();
+        QStringList pathList = QFileDialog::getOpenFileNames( this, "Open Files", path,tr("Image files (*.jpg *.png *.bmp)") );
+        ui->_listPath->addItems(pathList);
     }
-        break;
-    case SHOWIMAGE:{
-        int tmp;
-        tmp = ui->boxListImageOrigine->currentIndex();
-        _pathRaw = pathListRawFull.at(tmp).toStdString();
-       // _pathRaw = ui->boxListImageOrigine->currentText().toStdString();
-        tmp = ui->boxImageUnet->findText(ui->boxListImageOrigine->currentText());
-        if(tmp==-1){
-            QMessageBox msgBox2;
-            msgBox2.setText("Have no data of image Unet");
-            msgBox2.exec();
+    else if (res == QMessageBox::No){
+        QDir directory = QFileDialog::getExistingDirectory(this, "Select Directory");
+        QStringList pathList = directory.entryList(QStringList() << "*.jpg" << "*.JPG" << "*.png" << "*.PNG" << "*.bmp" << "*.BMP",QDir::Files);
+        QStringList pathListFull;
+        for (int i=0; i<pathList.size();i++){
+            pathListFull.append(directory.path()+"/"+pathList.at(i));
         }
-        else {
-         //   ui->boxImageUnet->setCurrentIndex(tmp);
-           // _pathUnet = ui->boxImageUnet->currentText().toStdString();
-            _pathUnet = pathListUnetFull.at(tmp).toStdString();
-            _paramProvider->setPathRaw(_pathRaw);
-            _paramProvider->setPathUnet(_pathUnet);
-            }
-        break;
+        ui->_listPath->addItems(pathListFull);
     }
-   case SAVEJSON:
-        if(ui->textDatasetJson->document()->isEmpty()){
-            QMessageBox msgBox3;
-            msgBox3.setText("Your file Dataset.json is empty !!!");
-            msgBox3.exec();
-        }
-        else{
-            QString path = QFileDialog::getSaveFileName(this, "Save Image", "",
-                                                                   "Json(*.json) ");
-            if(path == ""){
-              QMessageBox msgBox4;
-              msgBox4.setText("No directory");
-              msgBox4.exec();
-             }
-             else{
-                std::ofstream dataset(path.toStdString());
-                dataset<<ui->textDatasetJson->toPlainText().toStdString();
-                dataset.close();
-            }
-
-        }
-        break;
- }
 }
 
-void LoadFolderWidget::runUnet()
+void ShaderEditorWidget::showImage()
 {
-    QMessageBox msgBox;
-    switch(choisTypeRun){
-    case LOADDATA:{
-        msgBox.setText("Selection Folder Of Image after Segemation by Unet?");
-        msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-        msgBox.setDefaultButton(QMessageBox::No);
-        int res = msgBox.exec();
-        if (res==QMessageBox::Yes){
-            ui->boxImageUnet->clear();
-            QDir directory = QFileDialog::getExistingDirectory(this, "Select Directory");
-            QStringList nameList = directory.entryList(QStringList() << "*.jpg" << "*.JPG" << "*.png" << "*.PNG" << "*.bmp" << "*.BMP",QDir::Files);
-            for (int i=0; i<nameList.size();i++){
-                pathListUnetFull.append(directory.path()+"/"+nameList.at(i));
-            }
-            ui->boxImageUnet->addItems(nameList);
-        }
-        else if (res == QMessageBox::No){
-            QMessageBox msgBox1;
-            msgBox1.setText("Nothing Selected !!!");
-            msgBox1.exec();
-        }
-    }
-        break;
-    case SHOWIMAGE:{
-        int tmp;
-        tmp = ui->boxImageUnet->currentIndex();
-        _pathUnet = pathListUnetFull.at(tmp).toStdString();
-        //_pathUnet = ui->boxImageUnet->currentText().toStdString();
-        tmp = ui->boxListImageOrigine->findText(ui->boxImageUnet->currentText());
-        if(tmp==-1){
-            QMessageBox msgBox2;
-            msgBox2.setText("Have no data of image Raw");
-            msgBox2.exec();
-        }
-        else{
-           // ui->boxListImageOrigine->setCurrentIndex(tmp);
-            //_pathRaw = ui->boxListImageOrigine->currentText().toStdString();
-            _pathRaw = pathListRawFull.at(tmp).toStdString();
-            _paramProvider->setPathRaw(_pathRaw);
-            _paramProvider->setPathUnet(_pathUnet);
-        }
-        break;
-    }
-    case SAVEJSON:
-         break;
-    }
-
+    _path = ui->_listPath->currentText().toStdString();
+    //_paramProvider->setPath(_path);
 }
