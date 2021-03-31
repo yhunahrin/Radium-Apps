4b01b9d0b1ece4d958249a159f1f52ab25bebafd
yhunahrin <thienlangliettruyen@gmail.com>
2021-03-31 14:13:25 +0200

    Update MyParameterProvider.hpp

--- ShaderEditor/MyParameterProvider.hpp
+++ ShaderEditor/MyParameterProvider.hpp
@@ -6,7 +6,7 @@
 #include <QImageReader>
 #include <QSettings>
 #include <QString>
-
+#include "CameraManipulator.hpp"
 using ShaderConfigType = std::vector<std::pair<Ra::Engine::ShaderType, std::string>> ;
 
 class MyParameterProvider :public Ra::Engine::ShaderParameterProvider
@@ -17,28 +17,59 @@ public:
     void updateGL() override {
         // Method called before drawing each frame in Renderer::updateRenderObjectsInternal.
         // The name of the parameter corresponds to the shader's uniform name.
-        m_renderParameters.addParameter( "aColorUniform", m_colorParameter );
-        m_renderParameters.addParameter( "aScalarUniform", m_scalarParameter );
-        m_renderParameters.addParameter( "aTextureUniform",  m_textureManager.getOrLoadTexture(m_textureManager.addTexture(this->getPath(),0,0,nullptr)));
+       // m_renderParameters.addParameter( "aColorUniform", m_colorParameter );
+        m_renderParameters.addParameter( "aMixUniform", m_MixParameter );
+        m_textureRaw =  m_textureManagerRaw.getOrLoadTexture(m_textureManagerRaw.addTexture(this->getPathRaw(),0,0,nullptr));
+        m_textureUnet =  m_textureManagerUnet.getOrLoadTexture(m_textureManagerUnet.addTexture(this->getPathUnet(),0,0,nullptr));
+        m_textureRaw->bind();
+        m_textureUnet->bind();
+        m_renderParameters.addParameter( "aRawTextureUniform",m_textureRaw );
+        m_renderParameters.addParameter( "aUnetTextureUniform",m_textureUnet);
+        this->getMouse();
+        m_renderParameters.addParameter( "aMousePos",m_mouse);
     }
-    std::string getPath(){
-           return m_path;
+    std::string getPathRaw(){
+           return m_pathRaw;
        }
-    void setPath(std::string path){
-        m_path = path;
+
+    std::string getPathUnet(){
+           return m_pathUnet;
+       }
+
+    Ra::Core::Vector2 getMouse(){
+        return m_mouse;
     }
-    void setOrComputeTheParameterValues(std::string path) {
+    void setMouse( Ra::Core::Vector2 mouse){
+        m_mouse = mouse;
+    }
+    void setPathRaw(std::string pathRaw){
+        m_pathRaw = pathRaw;
+    }
+    void setPathUnet(std::string pathUnet){
+        m_pathUnet = pathUnet;
+    }
+    /*void setOrComputeTheParameterValues(std::string path) {
         // client side computation of the parameters, e.g.
         m_colorParameter  = Ra::Core::Utils::Color::Red();
         m_scalarParameter = .5_ra;
         m_path = path;
-    }
-
+    }*/
+  //  void clickMouse(QMouseEvent *event){
+    //    _camera->handleMousePressEvent(event,Qt::LeftButton,Qt::ShiftModifier,1);
+    //    m_mouse=_camera->getMouse();
+   // }
   private:
-    Ra::Core::Utils::Color m_colorParameter {Ra::Core::Utils::Color::Green()};
-    Scalar m_scalarParameter {1};
-    Ra::Engine::TextureManager m_textureManager{};
-    std::string m_path {""};
+  //  Ra::Core::Utils::Color m_colorParameter {Ra::Core::Utils::Color::Green()};
+    Scalar m_MixParameter {.5_ra};
+    Ra::Engine::TextureManager m_textureManagerRaw{};
+    Ra::Engine::TextureManager m_textureManagerUnet{};
+    Ra::Engine::Texture * m_textureRaw{};
+    Ra::Engine::Texture * m_textureUnet{};
+    std::string m_pathRaw {""};
+    std::string m_pathUnet {""};
+    QMouseEvent *event;
+    Ra::Core::Vector2 m_mouse{{0,0}};
+    CameraManipulator2D * _camera;
 };
 
 
